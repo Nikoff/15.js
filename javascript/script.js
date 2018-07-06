a51a08195719964d2ab305eb2895ecfbaac81331
Nikoff <j.ogorodnikov@gmail.com>
2018-07-06 07:25:48 +0300

    Added initial combination swapping

--- javascript/script.js
+++ javascript/script.js
@@ -17,10 +17,14 @@ for (i = 1; i <= 4; i++) {
 	for (j = 1; j <= 4; j++) {
 		var cell = document.createElement('td'); // Создаем 4 ячейки в каждом ряду, цикл (j), итог - таблица 4х4
 		cell.id = i + " "+ j;
+		cell.onclick = cellClick; // Привязываем функцию cellClick при клике на костяшку.
 		cell.textContent = arr[i][j];
 		console.log('cell.id = '+ cell.id+', cell.textContent='+cell.textContent);
 		row.appendChild(cell);// Добавляем ячейку в строку
 		table.appendChild(row);// Добавляем строку в итаблицу
+
+		if(box.childNodes.lengh == 1) box.removeChild(firstChild); // Удаляем таблицу, если играется повторно
+
 		box.appendChild(table);// Запихиваем в box table</div>	
 
 	}
@@ -32,21 +36,23 @@ function swap(arr, i1, j1, i2, j2) {
 	arr[i2][j2] = t;
 };
 
+function cellClick () {};
+
 function newGame() {
 	ei = 4; // Запоминаем индексы (координаты) пустышки
 	ej = 4; 
 	// Теперь задаем цикл, чтобы перемешать начальные костяшки, например 2000 раз 
 	for (var i = 0; i >= 2000; i++) {
 		 switch(Math.round(3*Math.random()+1)) {
-		 	case 1:    // замена наверх
+		 	case 1:  if(ej !=1) swap(arr,ei, ej, ei, ej--);  // замена наверх
 		 	break;
-		 	case 2:    // замена вправо
+		 	case 2:  if(ej !=4) swap(arr,ei, ej, ei++, ej);  // замена вправо
 		 	break;
-		 	case 3:    // замена вниз
+		 	case 3:  if(ej !=4) swap(arr,ei, ej, ei, ej++);  // замена вниз
 		 	break;
-		 	case 4:    // замена влево
+		 	case 4:  if(ej !=1) swap(arr,ei, ej, ei--, ej);  // замена влево
 		 };
-	}
+	};
 };
 
 
